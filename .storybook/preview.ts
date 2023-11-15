98f0e6d18d26a4b912b032c1bf03e6f9962d1899
Hung Luu <hungluu2106@gmail.com>
2023-11-15 10:49:14 +0700

    feat: add tailwind css

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,3 +1,5 @@
+import '../styles/tailwind.css'
+
 export const parameters = {
   backgrounds: {
     default: 'light',
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./stories/**/*.{ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
